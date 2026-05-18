dff8ef4e9099bb10f6d63c1422af47bb50a64da4
shishigu <santosjustine678@gmail.com>
2026-05-18 16:11:03 +0800

    Initial commit — ws03 project setup

---
+++ helpers.php
@@ -0,0 +1,49 @@
+<?php
+
+/**
+ * Get the base path
+ * 
+ * 
+ * @param string $path
+ * @return string
+ */
+
+function basePath($path = '')
+{
+    return __DIR__ . '/' . $path;
+}
+
+function loadView($name, $data = [])
+{
+    $viewPath = basePath("App/views/{$name}.view.php");
+
+    if (file_exists($viewPath)) {
+        extract($data);
+        require $viewPath;
+    } else {
+        echo "View {$name} not found!";
+    }
+}
+
+function loadPartial($name)
+{
+    $partialPath = basePath("App/views/partials/{$name}.php");
+
+    if (file_exists($partialPath)) {
+        require $partialPath;
+    } else {
+        echo "Partial {$name} not found";
+    }
+}
+
+function inspect($value)
+{
+    echo '<pre>';
+    var_dump($value);
+    echo '<pre>';
+}
+
+function formatSalary($salary)
+{
+    return '$' . number_format(floatval($salary));
+}
---
+++ public/index.php
@@ -0,0 +1,19 @@
+<?php
+
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+require '../helpers.php';
+require basePath('Framework/Router.php');
+require basePath('Framework/Database.php');
+
+$router = new Router();
+
+$routes = require basePath('routes.php');
+
+$uri = parse_url($_SERVER['REQUEST_URI'],PHP_URL_PATH);
+
+$method = $_SERVER['REQUEST_METHOD'];
+
+$router->route($uri, $method);
+
